b46068f509588fb6df21e49f8db99c810756562f
Crowdbotics <team@crowdbotics.com>
2023-02-23 16:44:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jolly-poetry-39086",
+  url: "https://jolly-poetry-39086.botics.co",
+  api: "https://jolly-poetry-39086.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
